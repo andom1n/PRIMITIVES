94056b7481b231b23c808f10e9176b11c3af3736
Andrei Tarasov <adnominator@bk.ru>
2022-11-09 21:02:22 +0500

    primitives

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int oneBonusMile = 20;
+        int ticket = 221;
+
+        int TotalBonus = ticket / oneBonusMile;
+        System.out.printf( "Итого миль начислится с билета: " + TotalBonus);
+
+    }
+}
